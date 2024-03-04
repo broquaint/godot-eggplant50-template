1f3574c66acc461c51e9ae8363ede46d95b05673
Dan Brook <dan@broquaint.com>
2024-03-04 20:52:40 +0700

    Add matching to match 3 game

--- games/third_times_a_charm/Main.gd
+++ games/third_times_a_charm/Main.gd
@@ -10,10 +10,22 @@ func _ready() -> void:
 			var s : Sprite = $Shapes.get_children()[randi() % $Shapes.get_child_count()].duplicate()
 			s.global_position = Vector2(32+(x+1) * 64, 32+(y+1) * 64)
 			s.scale = Vector2(0.9, 0.9)
+			s.set_meta("shape", _sprite_to_name(s))
 			$Grid.add_child(s)
 			
 			grid[x].insert(y, s)
 
+func _sprite_to_name(s: Sprite):
+	var rp = s.texture.resource_path
+	if rp.match("*square*"):
+		return "square"
+	elif rp.match("*triangle*"):
+		return "triangle"
+	elif rp.match("*circle*"):
+		return "circle"
+	elif rp.match("*hexagon*"):
+		return "hexagon"
+	
 func get_input() -> Dictionary:
 	var move := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	return {
@@ -47,6 +59,66 @@ func _process(_delta: float) -> void:
 			if input.move:
 				try_swap(cpos, move)
 
+func check_matches():
+	var matches = { h = [], v = [] }
+	for x in range(grid.size()):
+		for y in range(grid[x].size()):
+			var shape = grid[x][y].get_meta('shape')
+
+			# scan horizontally
+			var i = x
+			var h_matches = []
+			while i < grid.size() and grid[i][y].get_meta('shape') == shape:
+				h_matches.append(grid[i][y])
+				i += 1
+			if h_matches.size() >= 3:
+				matches.h.append(h_matches)
+
+			# scan horizontally
+			var j = y
+			var v_matches = []
+			while j < grid[x].size() and grid[x][j].get_meta('shape') == shape:
+				v_matches.append(grid[x][j])
+				j += 1
+			if v_matches.size() >= 3:
+				matches.v.append(v_matches)
+
+	for m in matches.h:
+		remove_match(m)
+	for m in matches.v:
+		remove_match(m)
+
+	fill_gaps(matches)
+
+const SWELL_PERIOD = 0.25
+const SHRINK_PERIOD = 0.2
+func remove_match(matched_shapes):
+	for s in matched_shapes:
+		# Prevent trying to remove the same shape twice
+		if s.has_meta('removing1'):
+			continue
+		var swell = get_tree().create_tween()
+		swell.tween_property(
+			s, "scale", Vector2(1.1, 1.1), SWELL_PERIOD
+		)
+		swell.set_ease(Tween.EASE_IN)
+		s.set_meta('removing1', true)
+
+	yield(get_tree().create_timer(SWELL_PERIOD), 'timeout')
+
+	for s in matched_shapes:
+		if s.has_meta('removing2'):
+			continue
+		var shrink = get_tree().create_tween()
+		shrink.tween_property(
+			s, "scale", Vector2.ZERO, SHRINK_PERIOD
+		)
+		shrink.set_ease(Tween.EASE_IN)
+		s.set_meta('removing2', true)
+	yield(get_tree().create_timer(SHRINK_PERIOD), 'timeout')
+
+func fill_gaps(matches):
+	pass
 const MOVE_PERIOD = 0.2
 func move_cursor(from, to):
 	$Cursor/Tween.interpolate_property(
@@ -56,6 +128,7 @@ func move_cursor(from, to):
 
 	yield($Cursor/Tween, 'tween_completed')
 	is_swapping = false
+	check_matches()
 
 func try_swap(move_from: Vector2, move_to: Vector2):
 	var from_x_idx = (move_from.x / 64) - 1
